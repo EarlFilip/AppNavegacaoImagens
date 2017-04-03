ce11462f4f74f83b0325bd3ee78729b68bfecb7e
Juan Filippo <juanfilippo0@gmail.com>
2017-04-03 01:02:39 -0300

    v1.0

--- app/src/main/java/com/example/athena/appnavegacaoimagens/MainActivity.java
+++ app/src/main/java/com/example/athena/appnavegacaoimagens/MainActivity.java
@@ -15,6 +15,7 @@ import java.util.List;
 
 public class MainActivity extends Activity {
     ImageView imageShow;
+    List<URL> Imagens;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,25 +24,24 @@ public class MainActivity extends Activity {
         imageShow = (ImageView) findViewById(R.id.imageShow);
     }
 
-    public void imagemAmostra(){}
-
     public void anterior(View v){
+        Toast.makeText(MainActivity.this, "Anterior", Toast.LENGTH_SHORT).show();
     }
 
     public void proximo(View v){
+        Toast.makeText(MainActivity.this, "Prooooximaa", Toast.LENGTH_SHORT).show();
     }
 
     public void loadImage(View v){
         DownloadAsyncImage process = new DownloadAsyncImage();
         try {
-            List<URL> todasImagens;
-            todasImagens = null;
-            todasImagens.add(new URL("http://southparkoculus.toolofnadrive.com/assets/imgs/global/10.jpg"));
-            todasImagens.add(new URL("http://esq.h-cdn.co/assets/16/45/980x490/landscape-1478792565-election-stan.png"));
-            todasImagens.add(new URL("http://www.southpark.com.br/wp-content/themes/simplo/images/south-park-header.jpg"));
-            todasImagens.add(new URL("https://i.ytimg.com/vi/ZrU_tt4R3xY/maxresdefault.jpg"));
-            todasImagens.add(new URL("http://www.indiewire.com/wp-content/uploads/2016/04/south-park-season-19.jpg"));
-            process.execute(todasImagens);
+            Imagens = null;
+            Imagens.add(new URL("http://southparkoculus.toolofnadrive.com/assets/imgs/global/10.jpg"));
+            Imagens.add(new URL("http://esq.h-cdn.co/assets/16/45/980x490/landscape-1478792565-election-stan.png"));
+            Imagens.add(new URL("http://www.southpark.com.br/wp-content/themes/simplo/images/south-park-header.jpg"));
+            Imagens.add(new URL("https://i.ytimg.com/vi/ZrU_tt4R3xY/maxresdefault.jpg"));
+            Imagens.add(new URL("http://www.indiewire.com/wp-content/uploads/2016/04/south-park-season-19.jpg"));
+            process.execute(Imagens);
         } catch (MalformedURLException e) {
             Toast.makeText(getApplicationContext(),"URL inválida",Toast.LENGTH_SHORT).show();
         }
@@ -51,7 +51,7 @@ public class MainActivity extends Activity {
         private ProgressDialog progressDialog;
         @Override
         protected void onPreExecute(){
-            progressDialog = ProgressDialog.show(MainActivity.this,"Carregando Imagem","Por favor aguarde...");
+            progressDialog = ProgressDialog.show(MainActivity.this,"Carregando Imagens","Por favor aguarde...");
         }
         @Override
         protected List<Bitmap> doInBackground(List<URL>... params) {
@@ -69,12 +69,13 @@ public class MainActivity extends Activity {
         }
         @Override
         protected void onPostExecute(List<Bitmap> bitmaps){
-            Bitmap bitmap;
-            bitmap = null;
-            if(bitmaps != null){
-                imageShow.setImageBitmap(bitmap);
+            for (Bitmap bitmap: bitmaps) {
+                if(bitmap != null){
+                    imageShow.setImageBitmap(bitmap);
+                }
+                progressDialog.dismiss();
             }
-            progressDialog.dismiss();
-        }
+            }
+
     }
 }
